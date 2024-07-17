fb1f706775c7d56af850bb9339f6b12519db8940
Capricorn14 <rhea9778@gmail.com>
2024-07-17 13:01:25 +0530

    Classes and pointers

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        Cookie cookieone = new Cookie("green");
+        Cookie cookieTwo = new Cookie("blue");
+
+        System.out.println(cookieone.getColor());
+        System.out.println(cookieTwo.getColor());
+    }
+    
+}
